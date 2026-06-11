bee7ca7d201d887f5f3be1bcb59035b91357e967
jacobtsang88 <jacobtsang88@gmail.com>
2026-06-11 13:47:51 -0400

    formatting and testing .gitignore

--- GoggleLookup.py
+++ GoggleLookup.py
@@ -1,31 +1,36 @@
 import json
 DEFAULT_LIBRARY = "goggle_library.json"
 
+#testing testing testing
+
 def load_library(filepath):
     with open(filepath, "r", encoding = "utf-8") as f:
         library = json.load(f)
     return library
 
 def find_goggles(library, wavelength) -> int:
-    results = []
+    goggleList = [] #add goggle, OD, notes
     for goggle in library:
         for band in goggle["bands"]:
             if(wavelength > band["start_nm"] and wavelength < band["stop_nm"]):
-                results.append(goggle["product"])
+                goggleList.append(goggle["product"])
                 break 
                 '''
                 alright, so essentially there are overlaps in the bands sometimes
                 which means i gotta just make sure theres only one band per goggle
                 but OD is diff for each band, even when theres overlap. (???)
                 '''
-    if results:
-        return results
+    if goggleList:
+        return goggleList
     else:
         return "there are no goggles that support this wavelength."
 
 def main():
     lib = load_library(DEFAULT_LIBRARY)
-    fart = int(input("input wavelength: "))
-    print(find_goggles(lib, fart))
+    tung = int(input("input wavelength: "))
+    rizz = find_goggles(lib, tung)
+    for i in rizz:
+        print(f"goggle: {i}")
+        
 
 main()
