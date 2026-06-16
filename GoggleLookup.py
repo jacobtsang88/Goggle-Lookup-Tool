ade720c50f617103ebd92ceb4962207526ded8b4
jacobtsang88 <jacobtsang88@gmail.com>
2026-06-16 13:51:18 -0400

    will now select higher OD goggle

--- GoggleLookup.py
+++ GoggleLookup.py
@@ -1,7 +1,6 @@
 import json
 DEFAULT_LIBRARY = "goggle_library.json"
 
-#testing testing testing
 
 def load_library(filepath):
     with open(filepath, "r", encoding = "utf-8") as f:
@@ -12,24 +11,27 @@ def find_goggles(library, wavelength) -> int:
     goggleList = [] #add goggle, OD, notes
     ODList = []
     for goggle in library:
+        '''
+        if theres 2 bands, for same goggle,
+        pick the higher one. then put in
+        jupyter notebook for ease of use. six seven.
+        '''
+        best_od = -1
         for band in goggle["bands"]:
             if(wavelength > band["start_nm"] and wavelength < band["stop_nm"]):
-                goggleList.append(goggle["product"])
-                ODList.append(band["od"])
-                break 
-                '''
-                if theres 2 bands, pick the higher one. then put in
-                jupyter notebook for ease of use. six seven.
-                '''
-    if goggleList:
-        return goggleList, ODList
-    else:
-        return "there are no goggles that support this wavelength."
+                if band["od"] > best_od:
+                    best_od = band["od"]
+        if best_od != -1:
+            goggleList.append(goggle["product"])
+            ODList.append(band["od"])
+    return goggleList, ODList
 
 def main():
     lib = load_library(DEFAULT_LIBRARY)
     tung = int(input("input wavelength: "))
     rizz, ODList = find_goggles(lib, tung)
+    if rizz or ODList == []:
+        print("no goggles found for that wavelength")
     for i in range(len(rizz)):
         print(f"goggle {i + 1}: {rizz[i]}, OD: {ODList[i]}")
 
